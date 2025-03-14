891a1eb531f4acd5edc6973f59c7f5fa97eba8f3
axelm-dev <axel.mainguy2022@campus-eni.fr>
2025-03-14 14:20:12 +0100

    integration openapi

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,103 @@
+import { Request, Response } from 'express';
+import UserService from '../services/user.service';
+
+class UserController {
+  private userService: UserService;
+
+  constructor() {
+    this.userService = new UserService();
+  }
+
+  // Créer un nouvel utilisateur
+  async createUser(req: Request, res: Response): Promise<void> {
+    try {
+      const userData = req.body;
+      const newUser = await this.userService.createUser(userData);
+      res.status(201).json(newUser);
+    } catch (error : any) {
+      res.status(400).json({ message: error.message });
+    }
+  }
+
+  // Obtenir un utilisateur par son ID
+  async getUserById(req: Request, res: Response): Promise<void> {
+    try {
+      const id = parseInt(req.params.id);
+      const user = await this.userService.getUserById(id);
+      if (user) {
+        res.json(user);
+      } else {
+        res.status(404).json({ message: 'Utilisateur non trouvé' });
+      }
+    } catch (error: any) {
+      res.status(500).json({ message: 'Erreur serveur' });
+    }
+  }
+
+  // Obtenir tous les utilisateurs
+  async getAllUsers(req: Request, res: Response): Promise<void> {
+    try {
+      //const users = await this.userService.getAllUsers();
+      const users = ['test']
+      res.json(users);
+    } catch (error: any) {
+      res.status(500).json({ message: 'Erreur serveur' });
+    }
+  }
+
+  // Mettre à jour un utilisateur
+  async updateUser(req: Request, res: Response): Promise<void> {
+    try {
+      const id = parseInt(req.params.id);
+      const userData = req.body;
+      const updatedUser = await this.userService.updateUser(id, userData);
+      res.json(updatedUser);
+    } catch (error: any) {
+      if (error.message === 'Utilisateur non trouvé') {
+        res.status(404).json({ message: error.message });
+      } else {
+        res.status(400).json({ message: error.message });
+      }
+    }
+  }
+
+  // Supprimer un utilisateur
+  async deleteUser(req: Request, res: Response): Promise<void> {
+    try {
+      const id = parseInt(req.params.id);
+      await this.userService.deleteUser(id);
+      res.status(204).send();
+    } catch (error: any) {
+      if (error.message === 'Utilisateur non trouvé') {
+        res.status(404).json({ message: error.message });
+      } else {
+        res.status(500).json({ message: 'Erreur serveur' });
+      }
+    }
+  }
+
+  // Rechercher des utilisateurs
+  async searchUsers(req: Request, res: Response): Promise<void> {
+    try {
+      const query = req.query.q as string;
+      const users = await this.userService.searchUsers(query);
+      res.json(users);
+    } catch (error: any) {
+      res.status(500).json({ message: 'Erreur serveur' });
+    }
+  }
+
+  // Paginer les utilisateurs
+  async paginateUsers(req: Request, res: Response): Promise<void> {
+    try {
+      const page = parseInt(req.query.page as string) || 1;
+      const perPage = parseInt(req.query.perPage as string) || 10;
+      const users = await this.userService.paginateUsers(page, perPage);
+      res.json(users);
+    } catch (error: any) {
+      res.status(500).json({ message: 'Erreur serveur' });
+    }
+  }
+}
+
+export default UserController;
---
+++ src/index.ts
@@ -0,0 +1,82 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import swaggerUiExpress from 'swagger-ui-express';
+import * as OpenApiValidator from 'express-openapi-validator';
+import path from "path";
+import fs from "fs";
+import * as yaml from 'yaml';
+
+
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("🚀 Sparkotto Backend is running!");
+});
+
+
+// Chemin vers le dossier des routes
+const routesPath = path.join(__dirname, 'routes');
+
+// Fonction pour charger dynamiquement les routes
+async function loadRoutes(routesPath: string) {
+  const files = fs.readdirSync(routesPath);
+
+  for (const file of files) {
+    const filePath = path.join(routesPath, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat.isFile() && (file.endsWith('.ts') || file.endsWith('.js'))) {
+      try {
+        const routeModule = await import(filePath);
+
+        // Vérifiez si le module exporte un routeur par défaut
+        if (routeModule.default && typeof routeModule.default === 'function') {
+          const router = routeModule.default;
+          const routeName = path.basename(file, path.extname(file)); // Nom de la route basé sur le nom du fichier
+          app.use(`/api/${routeName}`, router); // Préfixez avec /api et le nom de la route
+          
+          console.log(`Route chargée : /api/${routeName}`);
+        } else {
+          console.warn(`Fichier de route ignoré car il n'exporte pas de routeur par défaut : ${file}`);
+        }
+      } catch (error) {
+        console.error(`Erreur lors du chargement de la route ${file} :`, error);
+      }
+    }
+  }
+}
+// Load OpenAPI specification
+const openApiPath = path.join(__dirname, 'openapi.yaml');
+const openApiDocument = yaml.parse(fs.readFileSync(openApiPath, 'utf8'));
+
+// Serve Swagger UI
+app.use('/api-docs', swaggerUiExpress.serve, swaggerUiExpress.setup(openApiDocument));
+
+
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: openApiDocument,
+    validateRequests: true,
+    validateResponses: true,
+  })
+);
+
+
+// Chargement des routes au démarrage de l'application
+loadRoutes(routesPath).then(() => {
+  app.listen(PORT, () => {
+    console.log(`✅ Serveur lancé sur http://localhost:${PORT}`);
+  });
+});
+
+
+
