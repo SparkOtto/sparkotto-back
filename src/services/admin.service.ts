a3964b921506406c009f795f7c2e635f4182590d
Claire <claire.martin.gtc@gmail.com>
2025-04-23 16:33:23 +0200

    activer ou desactiver user
    # Conflicts:
    #	prisma/schema/user.prisma

---
+++ src/services/admin.service.ts
@@ -0,0 +1,22 @@
+import {User} from "@prisma/client";
+import UserDAO from "../dao/user.dao";
+
+class AdminService {
+    private userDAO: UserDAO;
+
+    constructor() {
+        this.userDAO = new UserDAO();
+    }
+
+    //Fonction pour activer ou désactiver un utilisateur
+    async toggleUserStatus(id: number, isActive: boolean): Promise<User>{
+        //S'assurer que l'ID de l'utilisateur existe en base
+        const userExist = await this.userDAO.getUserById(id);
+        if(!userExist){
+            throw new Error('L\'utilisateur n\'a pas été trouvé)');
+        }
+        const userData = {active: isActive};
+        return this.userDAO.updateUser(id, userData);
+    }
+}
+export default AdminService;
