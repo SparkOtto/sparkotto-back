9544818fef09d8a8d41b1560b9a1ad35a8791ebc
Claire <claire.martin.gtc@gmail.com>
2025-04-23 16:33:23 +0200

    activer ou desactiver user

---
+++ src/controllers/admin.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import AdminService from '../services/admin.service';
+
+class AdminController{
+    private adminService: AdminService;
+
+    constructor() {
+        this.adminService = new AdminService();
+    }
+
+    //Activer ou désactiver un utilisateur
+    async toggleUserStatus(req: Request, res: Response): Promise<Response>{
+        const id = parseInt(req.params.id);
+        const isActive = req.body;
+        try{
+            const updateUser = await this.adminService.toggleUserStatus(id, isActive);
+            return res.status(200).json(updateUser);
+        } catch (error){
+            return res.status(500).json({error: 'Erreur lors de la mise à jour de l\'utilisateur'});
+        }
+    }
+}
+export default AdminController;
