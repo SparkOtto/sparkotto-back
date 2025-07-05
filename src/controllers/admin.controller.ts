0bc02f84b4f44067da1bcbafecc26232e6d443f7
Claire <claire.martin.gtc@gmail.com>
2025-07-05 17:06:35 +0200

    Corrections email validation inscription

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -2,6 +2,7 @@ import {Request, Response} from "express";
 import AdminService from '../services/admin.service';
 import UserService from "../services/user.service";
 import EmailService from "../services/email.service";
+import jwt from "jsonwebtoken";
 
 class AdminController {
     private adminService: AdminService;
@@ -15,10 +16,38 @@ class AdminController {
     }
 
     /**
-     * Contrôler le mail du formulaire d'inscription
-     * Si domaine valide, envoi d'un mail pour validation
-     * Une fois validation faite, utilise toogleUserStatus pour activer le compte
+     * Si l'utilisateur demande à reçevoir un nouveau mail de confirmation
      */
+    async validUserWithEmail(req: Request, res: Response): Promise<Response> {
+        const { email, userId } = req.body;
+
+        if (!email || !userId || typeof userId != "number"){
+            return res.status(400).json({ error: "Email ou user invalid"})
+        }
+
+        try {
+            const result = await this.adminService.validateDomaine(email);
+
+            if (!result.valid) {
+                return res.status(400).json({ error: result.error });
+            }
+
+            // Récupère l'objet utilisateur pour l'utiliser dans sendToken
+            const user = await this.userService.getUserById(userId);
+
+            if (user){
+                // Envoyer l'email avec le lien de confirmation
+                await this.emailService.sendToken(user);
+            } else {
+                return res.status(404).json({ error: "Utilisateur non trouvé."})
+            }
+
+
+            return res.status(200).json({ message: "Email envoyé pour confirmation." });
+        } catch (error) {
+            return res.status(500).json({ error: "Erreur lors de la validation de l'email ou de l'envoi du mail." });
+        }
+    }
 
     /**
      * Activer ou désactiver un profil utilisateur
@@ -33,40 +62,6 @@ class AdminController {
             return res.status(200).json(updateUser);
         } catch (error) {
             return res.status(500).json({error: 'Erreur lors de la mise à jour de l\'utilisateur'});
-        }
-    }
-
-    /**
-     * Débloquer un utilisateur pour valider sa première inscription
-     * ou supprimer ses données en base si l'inscription est refusée
-     * @param req
-     * @param res
-     */
-    async activateUserAccount(req: Request, res: Response): Promise<Response> {
-        const id = parseInt(req.params.id);
-        const isApproved = req.body; // true pour inscription acceptée ou false si refusée
-        const updatedUser = await this.adminService.toggleUserStatus(id, true);
-
-        if (typeof isApproved !== 'boolean') {
-            throw new Error("Données invalides, si l'erreur persiste, veuillez contacter votre administrateur");
-        }
-
-        try {
-            if (isApproved) {
-                await this.emailService.sendConfirmationEmail(updatedUser); // email de validation
-            } else {
-                await this.emailService.sendRejectionEmail(updatedUser); // email de refus
-                await this.userService.deleteUser(id); // supprimer les données en bdd
-            }
-
-            return res.status(200).json({
-                message: isApproved
-                    ? 'Inscription validée, un email de confirmation a été envoyé à l\'utilisateur'
-                    : 'Inscription refusée, un email de confirmation a été envoyé à l\'utilisateur',
-                user: updatedUser,
-            });
-        } catch (error) {
-            return res.status(500).json({error: 'Erreur lors du taitement de l\'inscription de l\'utilisateur.'});
         }
     }
 }
