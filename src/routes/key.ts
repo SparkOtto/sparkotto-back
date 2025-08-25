f7d3d0fe354835586fad5e3f9e654281450abc76
axelm-dev <axel.mainguy2022@campus-eni.fr>
2025-08-25 11:06:21 +0200

    Add token on key.ts

--- src/routes/key.ts
+++ src/routes/key.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import KeyController from '../controllers/key.controller';
+import {authenticateToken} from "../middlewares/auth";
 
 const router = express.Router();
 const controller = new KeyController();
 
-router.post('/', controller.createKey.bind(controller));
-router.get('/', controller.getAllKeys.bind(controller));
-router.get('/:id_key', controller.getKeyById.bind(controller));
-router.put('/:id_key', controller.updateKey.bind(controller));
-router.delete('/:id_key', controller.deleteKey.bind(controller));
+router.post('/', authenticateToken, controller.createKey.bind(controller));
+router.get('/', authenticateToken, controller.getAllKeys.bind(controller));
+router.get('/:id_key', authenticateToken, controller.getKeyById.bind(controller));
+router.put('/:id_key', authenticateToken, controller.updateKey.bind(controller));
+router.delete('/:id_key', authenticateToken, controller.deleteKey.bind(controller));
 
 export default router;
