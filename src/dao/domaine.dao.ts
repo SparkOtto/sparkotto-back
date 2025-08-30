54873044081a00451cdf845160d1b2ee6c502246
Claire <claire.martin.gtc@gmail.com>
2025-08-30 16:34:44 +0200

    CRUD pour la gestion des domaines

--- src/dao/domaine.dao.ts
+++ src/dao/domaine.dao.ts
@@ -14,5 +14,41 @@ class DomaineDao {
         });
     }
 
+    // Récupérer tous les domaines
+    async getAllDomaines() {
+        return this.prisma.domaines.findMany();
+    }
+
+    //Récupérer un domaine par son ID
+    async getDomaineById(id: number) {
+        return this.prisma.domaines.findUnique({
+            where: {id_domaine: id}
+        });
+    }
+
+    // Ajouter un nouveau domaine
+    async addDomaine(domaine: string) {
+        return this.prisma.domaines.create({
+            data: {domaine_name: domaine}
+        });
+    }
+
+    // Mettre à jour un domaine existant
+    async updateDomaine(id: number, domaine: string) {
+        return this.prisma.domaines.update({
+            where: {id_domaine: id},
+            data: {domaine_name: domaine}
+        });
+    }
+
+    // Supprimer un domaine
+    async deleteDomaine(id: number) {
+        return this.prisma.domaines.delete({
+            where: {id_domaine: id}
+        });
+    }
+
+
+
 }
 export default DomaineDao
