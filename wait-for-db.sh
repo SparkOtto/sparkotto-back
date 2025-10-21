ae8fc68e562d78e05adb17e42189ee84b15918c2
axelm-dev <axel.mainguy2022@campus-eni.fr>
2025-10-21 14:42:35 +0100

    fix

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -9,16 +9,16 @@ echo "⏳ Attente de la base de données sur $DB_HOST:$DB_PORT..."
 until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" >/dev/null 2>&1; do
   sleep 2
 done
+ls -lah /app
+ls -lah /app/dist
+ls -lah /app/prisma
+
 echo "✅ Base de données prête !"
 
 npx prisma db push
 
 echo "🌱 Prisma seed..."
 npx prisma db seed
-
-ls -lah /app
-ls -lah /app/dist
-ls -lah /app/prisma
 
 echo "🚀 Lancement de l'application Node.js"
 node dist/index.js
